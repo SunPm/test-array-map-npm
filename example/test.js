56d1012d66aafef932a74bb7b69092bab5912c04
SunPm <15022352@qq.com>
2022-07-04 16:55:46 +0800

    添加测试

---
+++ example/test.js
@@ -0,0 +1,4 @@
+"use strict";
+exports.__esModule = true;
+var arrayMap = require("../dist/test-array-map-npm");
+console.log(arrayMap([1, 2], function (item) { return item + 2; }));
---
+++ example/test.ts
@@ -0,0 +1,3 @@
+import arrayMap = require('../dist/test-array-map-npm')
+
+console.log(arrayMap([1, 2], item => item + 2))
