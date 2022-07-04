589c670fa0e5d21ccf2484a25c2563dff6b5ca9e
SunPm <15022352@qq.com>
2022-07-04 16:49:11 +0800

    使用ts写法实现， 修复ts配置文件

--- test-array-map-npm.ts
+++ test-array-map-npm.ts
@@ -1,9 +1,11 @@
-const arrayMap = (array, callback) => {
+const arrayMap = (array: any[], callback: (item: any, index: number, arry: any[]) => any): any[] => {
   let i = -1;
   const len = array.length;
-  let resArray = [];
+  const resArray = [];
   while (++i < len) {
     resArray.push(callback(array[i], i, array));
   }
   return resArray;
 }
+
+export = arrayMap
